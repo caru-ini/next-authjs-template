d613cd372c9f7a625ef818ca7891d524f4856c56
caru-ini <caru17122@gmail.com>
2026-02-10 18:01:44 +0900

    chore(deps): upgrade next to v16 and react 19

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,32 @@
 import tailwind from 'eslint-plugin-tailwindcss';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import { globalIgnores,defineConfig } from "eslint/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({extends: ["next/core-web-vitals", "next/typescript"]}),
-  {
-    ignores: [
-      '**/.next/',
-    ],
+const eslintConfig = defineConfig([...nextVitals, ...nextTs, {
+  name: 'tailwindcss/recommended',
+  files: ['**/*.{js,jsx,ts,tsx}'],
+  plugins: {
+    tailwindcss: tailwind
   },
-  {
-    name: 'tailwindcss/recommended',
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    plugins: {
-      tailwindcss: tailwind
-    },
-    rules: {
-      ...tailwind.configs["recommended"].rules,
-      'tailwindcss/classnames-order': [
-        'warn',
-        {
-          callees: ['clsx', 'cn', 'twMerge']
-        }
-      ]
-    }
-  },
-  eslintPluginPrettierRecommended,
-];
+  rules: {
+    ...tailwind.configs["recommended"].rules,
+    'tailwindcss/classnames-order': [
+      'warn',
+      {
+        callees: ['clsx', 'cn', 'twMerge']
+      }
+    ]
+  }
+}, eslintPluginPrettierRecommended, globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]), {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}]);
 
 export default eslintConfig;
